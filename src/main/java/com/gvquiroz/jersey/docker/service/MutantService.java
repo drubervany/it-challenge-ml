d7bc18be3abd5157c70d4111c78090d363ba2205
gquiroz <gquiroz@oneloop.com.ar>
2017-11-15 23:12:57 -0300

    added first stub test . finish project bootstrap

--- src/main/java/com/gvquiroz/jersey/docker/service/MutantService.java
+++ src/main/java/com/gvquiroz/jersey/docker/service/MutantService.java
@@ -4,4 +4,9 @@ package com.gvquiroz.jersey.docker.service;
  * Created by gvquiroz on 15/11/17.
  */
 public class MutantService {
+
+    public boolean isMutant(){
+        return true;
+    }
+
 }
--- src/test/java/service/MutantServiceTest.java
+++ src/test/java/service/MutantServiceTest.java
@@ -1,7 +1,26 @@
 package service;
 
+import static org.junit.Assert.*;
+
+import com.gvquiroz.jersey.docker.service.MutantService;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by gvquiroz on 15/11/17.
  */
 public class MutantServiceTest {
+
+    private MutantService mutantService;
+
+    @Before
+    public void setup() {
+        mutantService = new MutantService();
+    }
+
+    @Test
+    public void testGetMessage() {
+        assertTrue(mutantService.isMutant());
+    }
+
 }
