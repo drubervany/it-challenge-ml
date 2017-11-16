5c29aacb83878e66c77aea3a324f8195467139e5
gquiroz <gquiroz@oneloop.com.ar>
2017-11-15 22:43:35 -0300

    added travis config

---
+++ src/main/java/com/gvquiroz/jersey/docker/service/MutantService.java
@@ -0,0 +1,7 @@
+package com.gvquiroz.jersey.docker.service;
+
+/**
+ * Created by gvquiroz on 15/11/17.
+ */
+public class MutantService {
+}
---
+++ src/test/java/service/MutantServiceTest.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * Created by gvquiroz on 15/11/17.
+ */
+public class MutantServiceTest {
+}
